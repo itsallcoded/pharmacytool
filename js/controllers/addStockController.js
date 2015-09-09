9b236bd8a4bb3799c5ecde280702cd2371fabcf0
itsallcoded <anshulsinha99@gmail.com>
2015-09-09 12:02:30 +0530

    committed

---
+++ js/controllers/addStockController.js
@@ -0,0 +1,7 @@
+define(['controllers/controllers'],
+    function (controllers) {
+        controllers.controller('addStockCtrl',
+            ['$scope', function($scope){
+            	console.log("addStockCtrl working");
+            }]);
+});
